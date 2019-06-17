93be29077c2716c3b852d7b3eaec61ac9a4d4ade
Your Name <27681135@qq.com>
2019-06-17 08:56:22 +0800

    引入mand normalize fastclick

--- src/main.js
+++ src/main.js
@@ -1,15 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import utils from './utility'
-import axios from 'axios'
-Vue.config.productionTip = false
+//为了跨平台样式正常引入normalize
+import "normalize.css";
+
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import utils from "./utility";
+import axios from "axios";
+
+//为解决safari中的bug引入fastclick
+import FastClick from "fastclick";
+if ("addEventListener" in document && "ontouchstart" in window) {
+  FastClick.prototype.focus = function(targetElement) {
+    targetElement.focus();
+  };
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      FastClick.attach(document.body);
+    },
+    false
+  );
+}
+//全局引入mand
+import mandMobile from "mand-mobile";
+import "mand-mobile/lib/mand-mobile.css";
+Vue.use(mandMobile);
+
+Vue.config.productionTip = false;
 Vue.prototype.$utils = utils;
 Vue.prototype.$axios = axios;
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/router.js
+++ src/router.js
@@ -1,22 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import map from './views/map.vue'
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+// import map from './views/map.vue'
+Vue.use(Router);
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'map',
-      component: map
+  routes: [
+    {
+      path: "/",
+      name: "map",
+      // component: map
+      component: () => import(/* webpackChunkName: "map" */ "./views/map.vue")
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
